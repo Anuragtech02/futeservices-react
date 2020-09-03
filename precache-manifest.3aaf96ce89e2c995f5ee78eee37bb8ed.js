82f85ffdd418a4a826087675b1096399759236b2
Anuragtech02 <apal895@gmail.com>
2020-09-03 10:27:55 +0530

    Updates

---
+++ precache-manifest.3aaf96ce89e2c995f5ee78eee37bb8ed.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3cd1a75443a8552910cff8623fc77b",
+    "url": "/futeservices-react/index.html"
+  },
+  {
+    "revision": "e21e6327d204a6a33c25",
+    "url": "/futeservices-react/static/css/main.8ddaf04e.chunk.css"
+  },
+  {
+    "revision": "9adee69754bb460979eb",
+    "url": "/futeservices-react/static/js/2.c4048048.chunk.js"
+  },
+  {
+    "revision": "134ac15c86dc35b1445bec132f50b552",
+    "url": "/futeservices-react/static/js/2.c4048048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e21e6327d204a6a33c25",
+    "url": "/futeservices-react/static/js/main.fc6a0622.chunk.js"
+  },
+  {
+    "revision": "74f022bbfff157aac021",
+    "url": "/futeservices-react/static/js/runtime-main.834cc66b.js"
+  },
+  {
+    "revision": "d4e2aea7f09589c077bf9ff2feb81064",
+    "url": "/futeservices-react/static/media/logo.d4e2aea7.png"
+  },
+  {
+    "revision": "8306298a6342d02146b91d438cb75b32",
+    "url": "/futeservices-react/static/media/pexels-artem-beliaikin-1319795.8306298a.jpg"
+  },
+  {
+    "revision": "35e46f85de3c4b3859850a322f00eca2",
+    "url": "/futeservices-react/static/media/pexels-bich-tran-669996.35e46f85.jpg"
+  },
+  {
+    "revision": "fc133ada12fedf4543525d72955a8acd",
+    "url": "/futeservices-react/static/media/pexels-simon-migaj-747964.fc133ada.jpg"
+  }
+]);
