d6271f38050f96ac969815456b1a4fc801f7443a
Anuragtech02 <apal895@gmail.com>
2020-09-03 10:57:13 +0530

    Updates

---
+++ precache-manifest.a54baf104d77103807ca0eec319386db.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7990021f7075b48fc806a8461c9a72e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e21e6327d204a6a33c25",
+    "url": "/static/css/main.8ddaf04e.chunk.css"
+  },
+  {
+    "revision": "9adee69754bb460979eb",
+    "url": "/static/js/2.c4048048.chunk.js"
+  },
+  {
+    "revision": "134ac15c86dc35b1445bec132f50b552",
+    "url": "/static/js/2.c4048048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e21e6327d204a6a33c25",
+    "url": "/static/js/main.fc6a0622.chunk.js"
+  },
+  {
+    "revision": "526f32a406e179ae8fbb",
+    "url": "/static/js/runtime-main.2b3827de.js"
+  },
+  {
+    "revision": "d4e2aea7f09589c077bf9ff2feb81064",
+    "url": "/static/media/logo.d4e2aea7.png"
+  },
+  {
+    "revision": "8306298a6342d02146b91d438cb75b32",
+    "url": "/static/media/pexels-artem-beliaikin-1319795.8306298a.jpg"
+  },
+  {
+    "revision": "35e46f85de3c4b3859850a322f00eca2",
+    "url": "/static/media/pexels-bich-tran-669996.35e46f85.jpg"
+  },
+  {
+    "revision": "fc133ada12fedf4543525d72955a8acd",
+    "url": "/static/media/pexels-simon-migaj-747964.fc133ada.jpg"
+  }
+]);
