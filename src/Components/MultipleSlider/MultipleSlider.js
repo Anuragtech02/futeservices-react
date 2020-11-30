00449e7f571ba699ea176eb3849a56dea921bc60
nancyiet <nancynancybansal@gmail.com>
2020-11-30 15:02:12 +0530

    home changes done

--- src/Components/MultipleSlider/MultipleSlider.js
+++ src/Components/MultipleSlider/MultipleSlider.js
@@ -8,24 +8,26 @@ import {Logos} from "../../Static";
 export const MultipleSlider = ()=>{
     const imgSlides = () =>  
     Logos.map(logo => (  
-      <div className={styles.sliderBase}>  
+     
        
-         <div className={styles.imgContainer}> <img  src= {logo.img} alt="logo" /></div>   
+         <div key={logo.key} className={styles.imgContainer}>
+            <img  src= {logo.img} alt="logo" />   
+         </div>
+            
            
-      </div>  
+      
     ));  
     return (  
-       <div className={styles.slider}>  
+      
             
                      
-        <Slider className={styles.centerSlide} 
+        <Slider className={styles.slider} 
          dots={false}  
             slidesToShow={4}  
             slidesToScroll={1}  
             autoplay={true}  
-             
             autoplaySpeed={1000}>{imgSlides()}</Slider>  
-        </div>  
+       
       );  
 }
 export default MultipleSlider;
--- src/Static/Logos.js
+++ src/Static/Logos.js
@@ -1,33 +1,36 @@
 import img1 from "../Assets/brands/logo-transparent.png";
-import img2 from "../Assets/brands/AsusTek-black-logo.png";
-import img3 from "../Assets/brands/1280px-Quechua_company_logo.png";
-import img4 from "../Assets/brands/58428e7da6515b1e0ad75ab5.png";
-import img5 from "../Assets/brands/Always-Logo.png";
-import img6 from "../Assets/brands/unilever.jpg";
-import img7 from "../Assets/brands/ford-logo-2003.png";
+import img2 from "../Assets/brands/1280px-Quechua_company_logo.png";
+import img3 from "../Assets/brands/58428e7da6515b1e0ad75ab5.png";
+import img4 from "../Assets/brands/Always-Logo.png";
+import img5 from "../Assets/brands/unilever.jpg";
+import img6 from "../Assets/brands/ford-logo-2003.png";
 
 
 export const Logos = [
     {
         img : img1,
+        key:1,
     },
     {
         img : img2,
+        key:2,
     },
     {
          img : img3,
+         key:3,
     },
     {
         img : img4,
+        key:4,
     },
     {
         img : img5,
+        key:5,
     },
     {
         img : img6,
+        key:6,
     },
-    {
-        img : img7,
-    },
+    
     
 ]
