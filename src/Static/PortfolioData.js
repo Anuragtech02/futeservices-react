aab0cdbe2733a9efea5b5166452c858fcfae74fd
Anuragtech02 <apal895@gmail.com>
2021-04-20 12:24:47 +0530

    teamslider responsive done

--- src/Static/PortfolioData.js
+++ src/Static/PortfolioData.js
@@ -1,8 +1,23 @@
 import cat1 from "../Assets/projects/cat1.webp";
+import cat1Image1 from "../Assets/projects/cat1/cat1-fute-1.webp";
+import cat1Image2 from "../Assets/projects/cat1/cat1-fute-2.webp";
+import cat1Image3 from "../Assets/projects/cat1/cat1-fute-3.webp";
+import cat1Image4 from "../Assets/projects/cat1/cat1-fute-4.webp";
+import cat1Image5 from "../Assets/projects/cat1/cat1-fute-5.webp";
+
 import cat2 from "../Assets/projects/cat2.webp";
-// import cat3 from "../Assets/projects/cat3.webp";
+import cat2Image1 from "../Assets/projects/cat1/cat1-fute-6.webp";
+import cat2Image2 from "../Assets/projects/cat1/cat1-fute-7.webp";
+import cat2Image3 from "../Assets/projects/cat1/cat1-fute-8.webp";
+import cat2Image4 from "../Assets/projects/cat1/cat1-fute-9.webp";
+import cat2Image5 from "../Assets/projects/cat1/cat1-fute-10.webp";
+
 import cat4 from "../Assets/projects/cat4.webp";
-// import cat5 from "../Assets/projects/cat5.webp";
+import cat3Image1 from "../Assets/projects/cat1/cat1-fute-11.webp";
+import cat3Image2 from "../Assets/projects/cat1/cat1-fute-12.webp";
+import cat3Image3 from "../Assets/projects/cat1/cat1-fute-13.webp";
+import cat3Image4 from "../Assets/projects/cat1/cat1-fute-14.webp";
+import cat3Image5 from "../Assets/projects/cat1/cat1-fute-15.webp";
 
 export const PortfolioData = [
   {
@@ -11,23 +26,23 @@ export const PortfolioData = [
     images: [
       {
         title: "This is title",
-        src: cat1,
+        src: cat1Image1,
       },
       {
         title: "This is title",
-        src: cat1,
+        src: cat1Image2,
       },
       {
         title: "This is title",
-        src: cat1,
+        src: cat1Image3,
       },
       {
         title: "This is title",
-        src: cat1,
+        src: cat1Image4,
       },
       {
         title: "This is title",
-        src: cat1,
+        src: cat1Image5,
       },
     ],
   },
@@ -37,23 +52,23 @@ export const PortfolioData = [
     images: [
       {
         title: "This is title",
-        src: cat2,
+        src: cat2Image1,
       },
       {
         title: "This is title",
-        src: cat2,
+        src: cat2Image2,
       },
       {
         title: "This is title",
-        src: cat2,
+        src: cat2Image3,
       },
       {
         title: "This is title",
-        src: cat2,
+        src: cat2Image4,
       },
       {
         title: "This is title",
-        src: cat2,
+        src: cat2Image5,
       },
     ],
   },
@@ -63,27 +78,23 @@ export const PortfolioData = [
     images: [
       {
         title: "This is title",
-        src: cat4,
+        src: cat3Image1,
       },
       {
         title: "This is title",
-        src: cat4,
+        src: cat3Image2,
       },
       {
         title: "This is title",
-        src: cat4,
+        src: cat3Image3,
       },
       {
         title: "This is title",
-        src: cat4,
+        src: cat3Image4,
       },
       {
         title: "This is title",
-        src: cat4,
-      },
-      {
-        title: "This is title",
-        src: cat2,
+        src: cat3Image5,
       },
     ],
   },
